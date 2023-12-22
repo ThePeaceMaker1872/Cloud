a00d0dd946661127ca5a07fc0f00ced9f82a886c
serafeim damaskos <serafeimdamaskos@gmail.com>
2023-12-22 17:56:13 +0000

    init commit

---
+++ cloud/cloud/src/main/java/com/example/cloud/Controller.java
@@ -0,0 +1,19 @@
+package com.example.cloud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+    
+}
+
+
